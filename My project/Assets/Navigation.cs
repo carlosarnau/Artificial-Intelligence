2d9f21a9e8fa3d564268740824c4ed8918c6713f
Carlos <arnau.carlos01@gmail.com>
2022-09-23 10:23:36 +0200

    Separating scripts

--- My project/Assets/Navigation.cs
+++ My project/Assets/Navigation.cs
@@ -3,22 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Navigation : MonoBehaviour
 {
     public GameObject target;
     public NavMeshAgent agent;
-    public float freq = 0f;
-    public float turnSpeed = 0.0f;
-    public float movSpeed = 0.0f;
-    public float acceleration = 0.1f;
-    public float turnAcceleration = 0.1f;
-    public float maxSpeed = 10.0f;
-    public float maxTurnSpeed = 2.0f;
-
-    Quaternion rotation;
-    Vector3 movement;
-
-    float stopDistance = 1.0f;
 
     // Start is called before the first frame update
     void Start()
